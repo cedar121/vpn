761f50ddc6eeb749a43f02bd6cbea7bc8a3f782f
nanodesu88 <archon.saratov@gmail.com>
2018-04-04 06:50:53 +0400

    upd

--- src/binder.js
+++ src/binder.js
@@ -2,9 +2,20 @@ const net = require('net');
 const utils = require('./utils');
 
 const mappings = [];
+const peers = [];
+const enstablishes = [];
+const connections = [];
 
 const server = net.createServer(socket => {
   console.log(`[binder] ${socket.remoteAddress}:${socket.remotePort} connected`);
+
+  connections.push({
+    remote: {
+      address: socket.remoteAddress,
+      port: socket.remotePort
+    },
+    socket
+  });
 
   socket.on('data', data => {
     const pkg = JSON.parse(data.toString());
@@ -38,10 +49,45 @@ server.on('error', err => {
   console.log(`[binder] event.error ${err}`);
 });
 
-function run(options = {}) {
-  server.listen(27000, '0.0.0.0');
+const me = {};
 
-  return Promise.resolve({});
+function findConnection(address, port) {
+  return connections.find(connection => connection.remote.address === address && connection.remote.port === port);
+}
+
+function establish(address, port) {
+  const connection = findConnection(address, port);
+
+  connection.socket.write(JSON.stringify({
+    opcode: 'establish',
+    data: {
+      address: me.ip,
+      myName: me.ip
+    }
+  }));
+}
+
+function run(options = {}) {
+  return new Promise((resolve, reject) => {
+    utils.getNetworkIP().then(ip => {
+      me.ip = ip;
+
+      server.listen(27000, '0.0.0.0');
+
+      resolve({
+        addPeer(address, port) {
+          peers.push({address, port});
+        },
+        update() {
+          peers.filter(peer => {
+            return true;
+          }).forEach(peer => {
+            establish(peer.address, peer.port);
+          });
+        }
+      });
+    });
+  });
 }
 
 module.exports = {
--- start.js
+++ start.js
@@ -4,7 +4,19 @@ const binder = require('./src/binder');
 const binderPort = 27000;
 
 binder.run().then(api => {
+  const peersArgument = process.argv.find(arg => arg.startsWith('--peers'));
 
+  if (peersArgument) {
+    const peers = peersArgument.substring('--peers='.length).split(',');
+
+    if (peers.length) {
+      peers.forEach(peer => {
+        api.addPeer(peer, 27000);
+      });
+
+      api.update();
+    }
+  }
 });
 
 if (_.includes(process.argv, '-o')) {
